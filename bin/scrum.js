08dfb0aad6036af40619d9a1e05275dd0eaf84ff
Mikael Kohlmyr <mikael.kohlmyr@triggeredmessaging.com>
2016-05-06 11:58:19 +0100

    updated & fixed minor issues

--- bin/scrum.js
+++ bin/scrum.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 
-const cli = require("../lib/utils/cli");
+const cli = require("../lib/cli");
 const commands = require("../lib/commands/index");
 
 cli.main(commands);
